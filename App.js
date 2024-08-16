5fd4f6346f485d8e4ebd22edbaf37e2e3c308fb8
BAW-CO <bridgetw.designs@gmail.com>
2024-08-16 12:29:51 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      If JS doesn't kill me it will make me stronger.
+      <h1>What up players</h1>
+    </div>
+  );
+}
+
+export default App;
